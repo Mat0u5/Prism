f7832a5a920f0ce4f753bc008746275a51ce1fdf
Mat0u5 <matous.danko@gmail.com>
2026-05-01 18:58:42 +0200

    Updated to 1.21.11

--- common/src/main/java/com/anthonyhilyard/prism/mixin/MinecraftMixin.java
+++ common/src/main/java/com/anthonyhilyard/prism/mixin/MinecraftMixin.java
@@ -12,9 +12,7 @@ import net.minecraft.client.Minecraft;
 @Mixin(Minecraft.class)
 public class MinecraftMixin
 {
-	@SuppressWarnings("resource")
-	@Inject(method = "runTick", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V",
-		args = { "ldc=gameRenderer" }), require = 1)
+	@Inject(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V"))
 	public void runTick(boolean tickWorld, CallbackInfo callbackInfo)
 	{
 		Minecraft instance = (Minecraft)(Object)this;
